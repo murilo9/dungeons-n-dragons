3d3594002ef356559d1f0eb2a28a32efa09672cd
Murilo <murilomatias@alunos.utfpr.edu.br>
2019-09-06 01:20:57 -0300

    Implementado estrutura básica

---
+++ src/components/Character.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>{{character.name}}</p>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+var props = {
+    character: Object
+}
+export default {
+    props: props
+}
+</script>
---
+++ src/components/CharactersTable.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="players-table">
+        <draggable v-model="characters" @start="drag=true" @end="drag=false">
+            <Character 
+            v-for="character in characters" 
+            :key="character.id" 
+            :character="character"/>
+        </draggable>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+import draggable from 'vuedraggable'
+import Character from './Character.vue'
+
+var data = () => {
+    return {
+        characters: [
+            {id: 0, name: 'João'}, {id: 1, name: 'Maria'}
+        ]
+    }
+}
+
+var components = {
+    draggable,
+    Character
+}
+
+export default {
+    data: data,
+    components: components
+}
+</script>
